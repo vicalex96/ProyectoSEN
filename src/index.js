9ef9618db16fd100bde6c9de7247a838175ee144
weledezma <50269214+weledezma@users.noreply.github.com>
2019-05-28 11:38:03 -0400

    Ent1-002: Eliminada carpeta node_modules y cargadas nuevamente las vistas
    Fueron agregadas las vistas: viewHome (inicio), viewLogin (ingeresar), viewRegister (para registrar usuarios), ViewUserAdministration (para modificar los usuarios).
    Eliminada Carpeta Node_modules

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ const express = require("express")
 const app = express()
 const path = require("path")
 
-//crea el servidor
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), () => {
-	console.log('Server en el puerto:', app.get('port'));
-});
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+	//crea el servidor
+	app.set('port', process.env.PORT || 3000);
+	app.listen(app.get('port'), () => {
+		console.log('Server en el puerto:', app.get('port'));
+	});
 
 
 
-//habre el archivo controlador de rutas del proyecto
-require(__dirname + '/server/routes.js')(app)
+	//habre el archivo controlador de rutas del proyecto
+	require(__dirname + '/server/routes.js')(app)
+
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'ejs');
+
+	app.use("/css", express.static(path.join(__dirname, '/views/css')));
+	app.use("/img", express.static(path.join(__dirname, '/views/img')));
