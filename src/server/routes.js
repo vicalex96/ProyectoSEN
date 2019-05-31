734b2fe572d115d6386b71848626b81ce98000ba
Vicalex2108 <vadejesus.16@est.ucab.edu.ve>
2019-05-31 19:14:45 -0400

    Ent1-002: restaurado de routers.js
    restaurando el archivo routers.js, que fue actualizado por error
    arreglado un caracter en minuscula de un nombre en la view login, que estaba en minuscula y debia estar en mayuscula, lo cual generaba errores con heroku en el despliegue

--- src/server/routes.js
+++ src/server/routes.js
@@ -4,78 +4,87 @@ const authHelpers = require('../auth/_helpers');
 const passport = require('../auth/local');
 
 module.exports = (app) => {
-
-  app.get('/home', function (req, res) {
-    if (req.isAuthenticated()) {
-      res.render('viewHome', {
-        logged: true,
-        supervisor: req.user.supervisor,
-        user: req.user
-      });
-    }else{
-      res.render('viewHomeUnLog', {
-        logged: false,
-        supervisor: false,
-      });
-    }
-  })
-
-  app.get('/login', function (req, res) {
-    if (req.isAuthenticated()) {
-      res.redirect('/perfil');
-    }else{
-      res.render('viewlogin');
-    }
-  })
-
-  app.post('/login', authHelpers.loginRedirect, (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
-      if (err) { handleResponse(res, 500, 'error'); }
-      if (!user) { req.flash('loginMessage', 'No User found') }
-      if (user) {
-        req.logIn(user, function (err) {
-          if (err) { handleResponse(res, 500, 'Error'); }
-          res.redirect('/perfil');
-        });
-      }
-    })(req, res, next);
-  });
-
-    app.get('/register', function (req, res) {
-      res.render('viewRegister')
+    app.get('/', function (req, res) {
+      res.redirect('/home')
     })
 
+    app.get('/home', function (req, res) {
+      if (req.isAuthenticated()) {
+        res.render('viewHome', {
+          logged: true,
+          supervisor: req.user.supervisor,
+          user: req.user
+        });
+      }else{
+        res.render('viewHomeUnLog', {
+          logged: false,
+          supervisor: false,
+        });
+      }
+    })
+
+    app.get('/login', function (req, res) {
+      if (req.isAuthenticated()) {
+        res.redirect('/perfil');
+      }else{
+        res.render('viewLogin');
+      }
+    })
+
+    app.post('/login', authHelpers.loginRedirect, (req, res, next) => {
+      passport.authenticate('local', (err, user, info) => {
+        if (err) { handleResponse(res, 500, 'error'); }
+        if (!user) { req.flash('loginMessage', 'No User found') }
+        if (user) {
+          req.logIn(user, function (err) {
+            if (err) { handleResponse(res, 500, 'Error'); }
+            res.redirect('/perfil');
+          });
+        }
+      })(req, res, next);
+    });
+
+    app.get('/perfil', function (req, res) {
+
+      if (req.isAuthenticated()) {
+        res.render('viewPerfil',{
+          logged: true,
+          user: req.user,
+        })
+      }else{
+        res.redirect('/login');
+      }
+    })
+
+    app.get('/register', function (req, res) {
+
+      if (req.isAuthenticated()) {
+        res.render('viewRegister',{
+          logged: true,
+          user: req.user,
+        })
+      }else{
+        res.redirect('/home');
+      }
+    })
   app.post('/register', (req, res, next)  => {
       return authHelpers.createUser(req, res)
       .then((response) => {
-        passport.authenticate('local', (err, user, info) => {
-          console.log('hola');
-          if (user) { handleResponse(res, 200, 'success'); }
-        })(req, res, next);
+      if (response) { res.redirect('/userAdministration');}
       })
-      .catch((err) => { console.log(err)
+      .catch((err) => {
         handleResponse(res, 500, 'error'); });
   });
 
-function handleResponse(res, code, statusMsg) {
-  res.status(code).json({status: statusMsg});
-}
+app.get('/logout', authHelpers.loginRequired, (req, res, next) => {
+      req.logout();
+      handleResponse(res, 200, res.redirect('/login'));
 
-app.get('/perfil', function (req, res) {
+    });
 
-  if (req.isAuthenticated()) {
-    res.render('viewHome',{
-      logged: true,
-      user: req.user,
-    })
-  }else{
-    res.redirect('/login');
-  }
-})
-
-app.get('/grafo', function (req, res) {
+app.get('/userAdministration', function (req, res) {
       if (req.isAuthenticated() && req.user.supervisor) {
-        res.render('viewHome',{
+        res.render('viewUserAdministration',{
           logged: true,
           user: req.user,
         })
@@ -83,15 +92,20 @@ app.get('/grafo', function (req, res) {
         res.redirect('/home')
       }
     })
-    app.get('/userAdministration', function (req, res) {
-      res.render('viewUserAdministration')
-    })
 
-    app.get('/', function (req, res) {
-      res.redirect('/home')
-    })
 
-app.get('/bitacora', function (req, res) {
+    app.get('/grafo', function (req, res) {
+          if (req.isAuthenticated() && req.user.supervisor) {
+            res.render('viewGrafo',{
+              logged: true,
+              user: req.user,
+            })
+          }else{
+            res.redirect('/home')
+          }
+        })
+
+    app.get('/bitacora', function (req, res) {
               if (req.isAuthenticated() && req.user.supervisor) {
                 res.render('viewBitacora',{
                   logged: true,
@@ -113,6 +127,13 @@ app.get('/bitacora', function (req, res) {
 
   function handleResponse(res, code, statusMsg) {
     res.status(code).json({status: statusMsg});
+  }
+
+  function isLoggedIn (req, res, next) {
+	   if (req.isAuthenticated()) {
+		     return next();
+	   }
+     res.redirect('/home');
   }
 
 }
