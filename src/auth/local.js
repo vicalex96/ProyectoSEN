1537ebe61f6a8d752ad6fee2a28c28b4ae265425
Vicalex2108 <vadejesus.16@est.ucab.edu.ve>
2019-05-31 00:38:33 -0400

    Ent1-002: ahora las cuentas se pueden bloquear
    Con lo establecido en el documento del proyecto se agrego una forma de bloquear las cuentas de usuario al equivocarse 3 veces

--- src/auth/local.js
+++ src/auth/local.js
@@ -11,13 +11,35 @@ init();
 
 passport.use(new LocalStrategy(options, (username, password, done) => {
   // check to see if the username exists
-  knex('usuario').where({ nombre_usuario: username  }).first()
+  knex('usuario').where({ nombre_usuario: username }).first()
   .then((usuario) => {
-    if (!usuario) return done(null, false,req.flash('loginMessage', 'Usuario no encontrado'));
+    var idup = usuario.id
+    var suma= usuario.contador+1
+
+    if (!usuario) {
+      return done(null, false,req.flash('loginMessage', 'Usuario no encontrado'))
+    }
+
+    if (usuario.bloqueado) {
+      return done(null, false,req.flash('loginMessage', 'Usuario Bloqueado'))
+    }
+
     if (!authHelpers.comparePass(password, usuario.contrasena)) {
+
+
+      if(usuario.contador  < 2 && !usuario.bloqueado ) {
+        knex('usuario').where({id: idup }).update({contador: suma}, ['id', 'contador']).then()
+      } else if (!usuario.bloqueado){
+        knex('usuario').where({ id: idup }).update({bloqueado: true, contador: 0}, ['id', 'bloqueado', 'contador']).then()
+        return done(null, false, req.flash('loginMessage', 'Usuario Bloqueado'));
+      }
+
       return done(null, false, req.flash('loginMessage', 'Contraseña no valida'));
+    } else if(usuario.contador != 0 && !usuario.bloqueado ) {
+        knex('usuario').where({id: idup }).update({contador: 0}, ['id', 'contador']).then()
+        return done(null, usuario);
     } else {
-      return done(null, usuario);
+      return done(null, false, req.flash('loginMessage', 'Usuario Bloqueado'));
     }
   })
   .catch((err) => { return done(err); });
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,13 +1,12 @@
-const HerokuUrl = 'postgres://jgdfnswemkqcdh:9ccfd43d91fcc63885ef1f5eee5cbc80acf19c0750693b4e007d04b73402d09c@ec2-54-221-198-156.compute-1.amazonaws.com:5432/d2nu9k9bj51786'
 
 module.exports = require('knex')({
   client: 'pg',
-  connection: HerokuUrl,
-  /*{
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '123456',
-    database : 'postgres'
-  },*/
-  useNullAsDefault: true}
-);
+  connection: {
+    host : 'ec2-54-221-198-156.compute-1.amazonaws.com',
+    user : 'jgdfnswemkqcdh',
+    password : '9ccfd43d91fcc63885ef1f5eee5cbc80acf19c0750693b4e007d04b73402d09c',
+    database : 'd2nu9k9bj51786'
+  },
+  useNullAsDefault: true
+
+});
